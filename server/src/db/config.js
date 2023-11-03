2d145b190a648755476c3ac75dda8aa21d5d2784
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-11-03 12:18:18 +0000

    config updated

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const { Sequelize, DataTypes, Model } = require("sequelize");
 
-const db = new Squelize({
+const db = new Sequelize({
   dialect: "sqlite",
   storage: path.join(__dirname, "db.sqlite"),
 });
