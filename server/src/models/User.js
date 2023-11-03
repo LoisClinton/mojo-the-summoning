01c4ebad00bc43e1fb39910e41431d79052a7348
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-11-03 11:35:09 +0000

    user model done

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,16 @@
 // create your User model here
+const { db, DataTypes, Model } = require("./db.js");
+
+class User extends Model {}
+
+User.init(
+  {
+    username: DataTypes.STRING,
+  },
+  {
+    sequalize: db,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
